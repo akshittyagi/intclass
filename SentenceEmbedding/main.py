9407a2462686c6b85549e87ee95568e767b4a838
Akshit Tyagi <akty.tyagi7@gmail.com>
2019-02-25 21:48:43 -0500

    Cosmetic changes

--- SentenceEmbedding/main.py
+++ SentenceEmbedding/main.py
@@ -6,7 +6,7 @@ from Model import SentenceEmbedder
 
 if __name__ == "__main__":
     dataset_loc = "../DataSets/top-dataset-semantic-parsing/"
-    data_train, data_dev, data_test = data_loader(dataset_loc)
+    data_train, data_dev, data_test = data_loader(dataset_loc, dataset='fb')
     sentence_embedder = SentenceEmbedder()
     sentence_embedder.train(data_train, data_dev)
     sentence_embedder.test(data_test)
