5e077b21ffad8e87a733025f7f2ff9a47aeedffa
Varun Sharma <varunsharmadps@gmail.com>
2019-02-26 01:30:21 -0500

    Implemented Clean_Data functionality for ATIS dataset.

--- SentenceEmbedding/Data.py
+++ SentenceEmbedding/Data.py
@@ -1,16 +1,18 @@
 import os
 import string
 import re
+import json
 
 from nltk.tokenize import word_tokenize
 import sklearn
 
 from utils import fb_top_intent
 
+
 class DataCleaner(object):
 
     def __init__(self, tokenization_type, dataset='fb'):
-        self.token_type = tokenization_type 
+        self.token_type = tokenization_type
         self.dataset_type = dataset
 
     def filterOutPunctuation(self, word):
@@ -57,8 +59,8 @@ class DataCleaner(object):
             '''
             (<tokenized sent>, <top intent>)
             '''
-            train = open(path+"train.tsv", 'r')
-            test = open(path+"test.tsv", 'r')
+            train = open(path + "train.tsv", 'r')
+            test = open(path + "test.tsv", 'r')
             for line in train:
                 curr_line = line.split("\t")
                 cleaned_line = self.cleanLine(line[1])
@@ -68,21 +70,37 @@ class DataCleaner(object):
                 curr_line = line.split("\t")
                 cleaned_line = self.cleanLine(line[1])
                 curr_y = fb_top_intent(curr_line[2])
-                tst.append((cleaned_line, curr_y)) 
+                tst.append((cleaned_line, curr_y))
             tr, dev = split_data(tr)
-        
+
         if self.dataset_type == 'atis':
             '''
             (<tokenized sent>, <top intent>)
             '''
-            pass
+            tr = read_json(path + 'atistrain.json')
+            dev = read_json(path + 'atisdev.json')
+            tst = read_json(path + 'atistest.json')
 
         return tr, dev, tst
 
+
+def read_json(file_name):
+    lst = []
+    with open(file_name) as f:
+        data = json.load(f)
+        for elem in data['body']:
+            curr_line = elem['text'].split()
+            cleaned_line = self.cleanLine(curr_line)
+            curr_y = elem['intent']
+            lst.append((cleaned_line, curr_y))
+    return lst
+
+
 def split_data(tr, split=0.8):
     N = len(tr)
-    tr, dev = tr[:(split*N)], tr[(split*N):]
+    tr, dev = tr[:(split * N)], tr[(split * N):]
     return tr, dev
+
 
 def data_loader(path, dataset='fb'):
     dataCleaner = DataCleaner(tokenization_type=1, dataset=dataset)
