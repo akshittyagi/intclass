53086e3c9f25a9a2510363749d4a06d554242c46
Varun Sharma <varunsharmadps@gmail.com>
2019-03-26 18:14:38 -0400

    Partially implemented a stacked LSTM.

--- SentenceEmbedding/Neural.py
+++ SentenceEmbedding/Neural.py
@@ -3,6 +3,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SingleLayer(nn.Module):
 
     def __init__(self, input_dim, output_dim):
@@ -13,6 +14,7 @@ class SingleLayer(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         return x
+
 
 class ThreeLayer(nn.Module):
 
@@ -30,3 +32,21 @@ class ThreeLayer(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return x
+
+
+class StackedLSTM(nn.Module):
+
+    def __init__(self, hidden_dim=1200, embedding_dim=300, output_dim, num_layers=3):
+        super(StackedLSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+        self.output_dim = output_dim
+        self.num_layers = num_layers
+
+        self.rnns = [nn.LSTM(self.embedding_dim if i == 0 else self.hidden_dim,
+                             self.hidden_dim, batch_first=True)
+                     for i in range(self.num_layers)]
+        self.out = nn.Linear(self.hidden_dim, self.output_dim)
+
+    def forward(self, x):
+        pass
