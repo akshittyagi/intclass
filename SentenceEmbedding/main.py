995e8456f021d742a1f937f1bd3dd3666fb9bc84
Akshit Tyagi <akty.tyagi7@gmail.com>
2019-03-29 14:06:07 -0400

    minor error

--- SentenceEmbedding/main.py
+++ SentenceEmbedding/main.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     dataset_loc = "../DataSets/top-dataset-semantic-parsing/"
     data_train, data_dev, data_test = data_loader(dataset_loc, dataset='fb')
     sentence_embedder = SentenceEmbedder(train_data=data_train, dev_data=data_dev, epochs=40)
-    sentence_embedder.train(data_train, data_dev, model_type='feed_forwardxw')
+    sentence_embedder.train(data_train, data_dev, model_type='feed_forward')
     sentence_embedder.test(data_test)
