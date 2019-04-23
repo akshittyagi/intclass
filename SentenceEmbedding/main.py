f926fea09cd5dd4edcdddd6459e1d59adb4d65b6
Lynn Samson <lnsamsn@gmail.com>
2019-04-22 20:39:25 -0400

    Implement BERT pretraining

--- SentenceEmbedding/main.py
+++ SentenceEmbedding/main.py
@@ -16,19 +16,22 @@ if __name__ == "__main__":
     #### FB ####
     dataset_loc = "../DataSets/top-dataset-semantic-parsing/"
     data_train, data_dev, data_test = data_loader(dataset_loc, dataset='fb')
-    epoch = 4
+    epochs = 5
     # sentence_embedder = SentenceEmbedder(train_data=data_train, dev_data=data_dev, epochs=epoch, batch_size=128, debug=True)
     # sentence_embedder.train(data_train, data_dev, model_type='recurrent_bn')
     # sentence_embedder.test(data_test, model_type='recurrent_bn')
 
-    for ep in [5, 10, 15]:
-        for cl in [0, 0.5, 1, 5, 10, 50, 100]:
-            sentence_embedder = SentenceEmbedder(train_data=data_train, dev_data=data_dev, epochs=ep, batch_size=128, debug=True)
-            print('Training for {} epochs with clipping parameter {}'.format(ep, cl))
-            sentence_embedder.train(data_train, data_dev, clip=cl, model_type='recurrent_bn')
-            sentence_embedder.test(data_test, model_type='recurrent_bn')
-            os.remove('bn_av_sent_emb_3_layer_glove.MODEL')
-            print('File removed')
+    # for ep in [5, 10, 15]:
+    #     for cl in [0, 0.5, 1, 5, 10, 50, 100]:
+    #         sentence_embedder = SentenceEmbedder(train_data=data_train, dev_data=data_dev, epochs=ep, batch_size=128, debug=True)
+    #         print('Training for {} epochs with clipping parameter {}'.format(ep, cl))
+    #         sentence_embedder.train(data_train, data_dev, clip=cl, model_type='recurrent_bn')
+    #         sentence_embedder.test(data_test, model_type='recurrent_bn')
+    #         os.remove('bn_av_sent_emb_3_layer_glove.MODEL')
+    #         print('File removed')
+
+    sentence_embedder = SentenceEmbedder(train_data=data_train, dev_data=data_dev, epochs=epochs, batch_size=128, debug=True)
+    sentence_embedder.train_bert()
 
     # dataset_loc = "../DataSets/top-dataset-semantic-parsing/"
     # data_train, data_dev, data_test = data_loader(dataset_loc, dataset='fb')
